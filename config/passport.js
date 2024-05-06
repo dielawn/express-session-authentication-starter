5ac3490d68fdcd80b13dd046e02e890d5bc4ef5c
dielawn <dielawnmerkill@gmail.com>
2024-05-06 06:05:43 -0600

    user create, login, logout

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const connection = require('./database');
-
 const User = connection.models.User;
 const validPassword = require('../lib/passwordUtils').validPassword;
 
@@ -16,12 +15,13 @@ const verifyCallback = (username, password, done) => {
     User.findOne({ username: username })
         .then((user) => {
             //no error, user undefined
+            console.log(user)
             if (!user) { 
                 return done(null, false) 
             } 
 
             const isValid = validPassword(password, user.hash, user.salt);
-
+            console.log(isValid)
             if (isValid) {
                 //no error, user is valid
                 return done(null, user);
@@ -40,7 +40,7 @@ const strategy = new LocalStrategy(customfields, verifyCallback);
 passport.use(strategy);
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user._id);
 });
 
 passport.deserializeUser((userId, done) => {
